5b1f4f1aca09e936590e1c652bb7e013827a564c
ShashankMangal <shashankmangal10@gmail.com>
2025-04-30 09:42:32 +0530

    Remove unused imports in main.dart for cleaner code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
