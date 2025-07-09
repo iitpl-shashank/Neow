927b2c116e0b984b5eefc04da507e8dc361daa9d
iitpl-shashank <shashank@invoidea.net>
2025-07-09 11:40:52 +0530

    feat: simplify UI interaction by replacing nested GestureDetector and enhancing container structure

--- lib/ui/naveli_ui/home/myth_vs_facts/myth_vs_facts_view.dart
+++ lib/ui/naveli_ui/home/myth_vs_facts/myth_vs_facts_view.dart
@@ -162,32 +162,31 @@ class _MythVsFactsViewState extends State<MythVsFactsView> {
                   itemBuilder: (context, index) {
                     return Padding(
                       padding: const EdgeInsets.symmetric(vertical: 10),
-                      child: Container(
-                        width: kDeviceWidth / 1,
-                        clipBehavior: Clip.antiAlias,
-                        decoration: ShapeDecoration(
-                          color: CommonColors.mWhite,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
+                      child: GestureDetector(
+                        onTap: () {
+                          push(
+                            YouKnowView(
+                              position: index,
+                            ),
+                          );
+                        },
+                        child: Container(
+                          width: kDeviceWidth / 1,
+                          clipBehavior: Clip.antiAlias,
+                          decoration: ShapeDecoration(
+                            color: CommonColors.mWhite,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            shadows: const [
+                              BoxShadow(
+                                color: Color(0x3F000000),
+                                blurRadius: 8,
+                                offset: Offset(0, 2),
+                                spreadRadius: 0,
+                              )
+                            ],
                           ),
-                          shadows: const [
-                            BoxShadow(
-                              color: Color(0x3F000000),
-                              blurRadius: 8,
-                              offset: Offset(0, 2),
-                              spreadRadius: 0,
-                            )
-                          ],
-                        ),
-                        child: GestureDetector(
-                          onTap: () {
-                            // Navigator.of(context)
-                            push(
-                              YouKnowView(
-                                position: index,
-                              ),
-                            );
-                          },
                           child: Row(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             mainAxisAlignment: MainAxisAlignment.center,
@@ -231,76 +230,37 @@ class _MythVsFactsViewState extends State<MythVsFactsView> {
                               ),
                               if (mViewModel.postsList[index].fileType ==
                                   'image')
-                                GestureDetector(
-                                  onTap: () {
-                                    Navigator.of(context)
-                                        .push(MaterialPageRoute<void>(
-                                      fullscreenDialog: true,
-                                      builder: (BuildContext context) {
-                                        return GestureDetector(
-                                          onTap: () {
-                                            Navigator.pop(context);
-                                          },
-                                          child: Image.network(
-                                            height: 100,
-                                            width: 100,
-                                            mViewModel.postsList[index].posts ??
-                                                "https://icon-library.com/images/no-picture-available-icon/no-picture-available-icon-1.jpg",
-                                            fit: BoxFit.fitHeight,
-                                          ),
-                                        );
-                                      },
-                                    ));
-                                  },
-                                  child: Container(
-                                    height: 80,
-                                    width: 80,
-                                    margin: const EdgeInsets.only(
-                                      top: 10,
-                                      bottom: 10,
-                                    ),
-                                    decoration: BoxDecoration(
-                                      border: Border.all(
-                                          color: CommonColors.mGrey
-                                              .withOpacity(0.6),
-                                          width: 1),
-                                      borderRadius: BorderRadius.circular(10),
-                                      image: DecorationImage(
-                                        image: NetworkImage(mViewModel
-                                                .postsList[index].posts ??
-                                            "https://icon-library.com/images/no-picture-available-icon/no-picture-available-icon-1.jpg"),
-                                        fit: BoxFit.contain,
-                                      ),
+                                Container(
+                                  height: 80,
+                                  width: 80,
+                                  margin: const EdgeInsets.only(
+                                    top: 10,
+                                    bottom: 10,
+                                  ),
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color:
+                                            CommonColors.mGrey.withOpacity(0.6),
+                                        width: 1),
+                                    borderRadius: BorderRadius.circular(10),
+                                    image: DecorationImage(
+                                      image: NetworkImage(mViewModel
+                                              .postsList[index].posts ??
+                                          "https://icon-library.com/images/no-picture-available-icon/no-picture-available-icon-1.jpg"),
+                                      fit: BoxFit.contain,
                                     ),
                                   ),
                                 ),
                               if (mViewModel.postsList[index].fileType ==
                                   'link')
-                                // Text(
-                                //   mViewModel.postsList[index].posts ?? 'no video',
-                                //   style: getAppStyle(
-                                //     color: CommonColors.mGrey,
-                                //     fontSize: 16,
-                                //     fontWeight: FontWeight.w400,
-                                //     height: 1,
-                                //   ),
-                                // ),
-                                //   VideoPlayerScreen(
-                                //     link: mViewModel.postsList[index].posts ?? "https://icon-library.com/images/no-picture-available-icon/no-picture-available-icon-1.jpg",
-                                //     // isButtonVisible: true,
-                                //     isFillAvailableSpace: false,
-                                //     isLoop: true,
-                                //     isMute: true,
-                                //   ),
                                 SizedBox(
                                   height: 90,
                                   width: 90,
-                                  child: VideoPlayerScreen(
-                                    link: mViewModel.postsList[index].posts ??
-                                        "https://icon-library.com/images/no-picture-available-icon/no-picture-available-icon-1.jpg",
-                                    // isFillAvailableSpace: false,
-                                    // isLoop: true,
-                                    // isMute: false,
+                                  child: IgnorePointer(
+                                    child: VideoPlayerScreen(
+                                      link: mViewModel.postsList[index].posts ??
+                                          "https://icon-library.com/images/no-picture-available-icon/no-picture-available-icon-1.jpg",
+                                    ),
                                   ),
                                 ),
                             ],
@@ -310,83 +270,6 @@ class _MythVsFactsViewState extends State<MythVsFactsView> {
                     );
                   },
                 ),
-                // ListView.builder(
-                //   physics: NeverScrollableScrollPhysics(),
-                //   itemCount: mViewModel.postsList.length,
-                //   shrinkWrap: true,
-                //   itemBuilder: (context, index) {
-                //     return Padding(
-                //       padding: const EdgeInsets.symmetric(vertical: 5),
-                //       child: Container(
-                //         width: 390,
-                //         height: kDeviceHeight / 3,
-                //         clipBehavior: Clip.antiAlias,
-                //         decoration: ShapeDecoration(
-                //           color: CommonColors.mWhite,
-                //           shape: RoundedRectangleBorder(
-                //             borderRadius: BorderRadius.circular(10),
-                //           ),
-                //           shadows: [
-                //             BoxShadow(
-                //               color: Color(0x3F000000),
-                //               blurRadius: 8,
-                //               offset: Offset(0, 2),
-                //               spreadRadius: 0,
-                //             )
-                //           ],
-                //         ),
-                //         child: Column(
-                //           children: [
-                //             Container(
-                //               width: 390,
-                //               height: kDeviceHeight / 4.5,
-                //               decoration: BoxDecoration(
-                //                 image: DecorationImage(
-                //                   image: NetworkImage(mViewModel.postsList[index].posts ?? "https://icon-library.com/images/no-picture-available-icon/no-picture-available-icon-1.jpg"),
-                //                   fit: BoxFit.cover,
-                //                 ),
-                //               ),
-                //               child: Align(
-                //                 alignment: Alignment.topRight,
-                //                 child: Padding(
-                //                   padding: const EdgeInsets.all(8.0),
-                //                   child: Container(
-                //                     decoration: BoxDecoration(
-                //                         shape: BoxShape.circle,
-                //                         color: CommonColors.mWhite
-                //                     ),
-                //                     child: Padding(
-                //                       padding: const EdgeInsets.all(4.0),
-                //                       child: Icon(
-                //                         Icons.favorite_outlined,
-                //                         size: 25,
-                //                         color: CommonColors.greenColor,
-                //                       ),
-                //                     ),
-                //                   ),
-                //                 ),
-                //               ),
-                //             ),
-                //             Flexible(
-                //               child: Padding(
-                //                 padding: const EdgeInsets.all(10.0),
-                //                 child: Text(
-                //                   mViewModel.postsList[index].description!,
-                //                   style: getAppStyle(
-                //                     color: CommonColors.mGrey,
-                //                     fontSize: 16,
-                //                     fontWeight: FontWeight.w400,
-                //                     height: 1,
-                //                   ),
-                //                 ),
-                //               ),
-                //             )
-                //           ],
-                //         ),
-                //       ),
-                //     );
-                //   },
-                // ),
               ],
             ),
           ),
